ffd99c3baca3cf337ef1545b343bee496b8b6112
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-07-09 01:36:42 +0530

    our-first-test-code

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,15 @@
+import { render,screen } from "@testing-library/react"
+import Greeting from "./Greeting"
+
+test('render hello world as a test' ,()=>{
+    //arrange
+    render(<Greeting />);
+
+    //act
+    //nothing to do now
+
+
+    //assert
+   const elementGet =  screen.getByText('Hello wrld' , {exact:false})              //exact-true match lower and upper case to i.e exact by default its exact
+   expect(elementGet).toBeInTheDocument();                                                                           //expect its globally available
+})
